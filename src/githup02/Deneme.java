d6385f32de18dab15d0543c3350b726fd9278a88
mrtgklp <109852097+mrtgklp@users.noreply.github.com>
2023-02-25 19:28:19 +0300

    v3

--- src/githup02/Deneme.java
+++ src/githup02/Deneme.java
@@ -4,6 +4,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("Hi EveryBody");
         System.out.println("Worlds");
+        System.out.println("B129");
     }
 }
 //restore-->Tamamen working space i temizler
