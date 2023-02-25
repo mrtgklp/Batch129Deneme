6065154b38bb1cb63a316fa17fd8670edd2677a1
mrtgklp <109852097+mrtgklp@users.noreply.github.com>
2023-02-25 18:44:49 +0300

    v2

--- src/githup02/Deneme.java
+++ src/githup02/Deneme.java
@@ -3,5 +3,8 @@ package githup02;
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Hi EveryBody");
+        System.out.println("Worlds");
     }
 }
+//restore-->Tamamen working space i temizler
+//stash-->Hafızaya alır
